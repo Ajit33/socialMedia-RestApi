7e77744afda8dcc8d78fb00436614209bd35a62e
Ajit33 <justajit33@gmail.com>
2024-09-07 05:36:29 +0530

    "docker file added"

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,12 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/api/v1", rootRouter);
 app.use(errorHandler)
-
+app.length("/",(req,res)=>{
+    res.json({
+        message:"hello docker"
+    })
+})
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, async () => {
     await ConnectDatabase();
     console.log(`App is running on port ${PORT}`);
